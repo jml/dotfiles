9dee22bf2fca0eef6be8599b60055268b5d9b55b
Jonathan Lange <jml@mumak.net>
2015-08-09 14:59:26 +0100

    Update for OS X

--- bash/profile.d/01-aliases.sh
+++ bash/profile.d/01-aliases.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 # enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
+if hash dircolors 2>/dev/null; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
     alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
 
     alias grep='grep --color=auto'
     alias fgrep='fgrep --color=auto'
@@ -17,9 +15,11 @@ alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
 
-alias t=hodor
+if hash hodor 2>/dev/null; then
+    alias t=hodor
+fi
 
-if [ -x /usr/bin/apt-get ]; then
+if hash apt-get 2>/dev/null; then
     alias sagu='sudo apt-get update'
     alias sagi='sudo apt-get install'
 fi
--- bash/profile.d/02-glob-star.sh
+++ bash/profile.d/02-glob-star.sh
@@ -1,3 +1,5 @@
 # If set, the pattern "**" used in a pathname expansion context will
 # match all files and zero or more directories and subdirectories.
+
+# Requires bash 4, at least.
 shopt -s globstar
