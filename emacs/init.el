068e73f4ee78a2d64bcad0f71ed4db7f2ac9adcf
Jonathan Lange <jml@mumak.net>
2019-07-16 20:31:22 +0100

    Don't need blackout-on-tty any more
    I hardly use tty, and I get a black background anyway.

--- emacs/init.el
+++ emacs/init.el
@@ -17,12 +17,6 @@
 ;; Load manually-managed third-party plugins.
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins"))
 
-(defun blackout-on-tty (frame)
-  (if (not (window-system frame))
-      (set-face-background 'default "#000000" frame)))
-
-(add-hook 'after-make-frame-functions 'blackout-on-tty)
-
 ;; Customize
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
