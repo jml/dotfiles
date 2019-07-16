1522451e09eb4853d636bd0bb6d3941f297168cb
Jonathan Lange <jml@mumak.net>
2019-07-16 22:36:54 +0100

    Shuffle things around

--- emacs/config.d/20-packages.el
+++ emacs/config.d/20-packages.el
@@ -3,13 +3,93 @@
   :ensure t
   :config (exec-path-from-shell-initialize))
 
+(use-package multiple-cursors
+  :bind (("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-c C-<" . mc/mark-all-like-this)
+         ("C-M-c" . mc/edit-lines)))
+
+(use-package diff-hl
+  :config (global-diff-hl-mode))
+
+;; TODO: Update this to use bind-keymap
+(use-package popwin
+  :config (progn
+            (popwin-mode 1)
+            (global-set-key (kbd "C-z") popwin:keymap)))
+
+;; Helm
+
+(use-package helm
+  :config
+  (helm-mode 1))
+
+(use-package helm-config
+  :after helm
+  :bind (("M-x" . helm-M-x)
+         ("C-x C-f" . helm-find-files)))
+
+(use-package helm-ag
+  :ensure t)
+
+;; Projectile
+
+;; TODO: Express that this depends on helm-projectile?
+(use-package projectile
+  :ensure t
+  :bind ("C-c h" . helm-projectile)
+  :bind-keymap ("C-c p" . projectile-command-map)
+  :config
+  (projectile-global-mode)
+  (helm-projectile-on)
+  (setq projectile-use-git-grep 1))
+
+;; Magit
+
+(use-package magit
+  :bind (("\C-c g g" . magit-status)
+         ("<f12>" . magit-status))
+  :config (setq magit-last-seen-setup-instructions "1.4.0"))
+
+;; Flycheck
+
 (use-package flycheck
   :ensure t)
 
-(use-package flycheck-haskell
+;; LSP
+
+(use-package lsp-mode
+  :hook (prog-mode . lsp)
+  :commands lsp)
+
+(use-package lsp-ui :commands lsp-ui-mode)
+
+(use-package company-lsp
   :ensure t
-  :after flycheck
-  :hook (flycheck-mode . flycheck-haskell-setup))
+  :commands company-lsp)
+
+(use-package company-box
+  :ensure t
+  :hook (company-mode . company-box-mode))
+
+(use-package helm-lsp
+  :ensure t
+  :commands helm-lsp-workspace-symbol)
+
+;; TODO: Investigate DAP mode
+(use-package dap-mode)
+
+;; Snippets
+(use-package yasnippet
+  :ensure t
+  :config (yas-global-mode))
+
+(use-package yasnippet-snippets
+  :ensure t)
+
+;; LANGUAGES
+
+;; Go
 
 (use-package flycheck-gometalinter
   :ensure t
@@ -24,11 +104,18 @@
   :hook (before-save . gofmt-before-save)
   :bind (:map go-mode-map ([remap xref-find-definitions] . godef-jump)))
 
+;; Haskell
+
 (use-package haskell-mode
   :ensure t
   :hook ((haskell-mode . turn-on-haskell-indentation)
          (haskell-mode . turn-on-haskell-doc)
          (haskell-mode . turn-on-haskell-decl-scan)))
+
+(use-package flycheck-haskell
+  :ensure t
+  :after flycheck
+  :hook (flycheck-mode . flycheck-haskell-setup))
 
 (use-package intero
   :ensure t
@@ -36,37 +123,13 @@
   :hook (haskell-mode . intero-mode)
   :config (flycheck-add-next-checker 'intero '(warning . haskell-hlint)))
 
-(use-package helm
-  :config
-  (helm-mode 1))
-
-(use-package helm-config
-  :after helm
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)))
-
-(use-package lsp-mode
-  :hook (prog-mode . lsp)
-  :commands lsp)
-
-;; optionally
-(use-package lsp-ui :commands lsp-ui-mode)
-(use-package company-lsp :commands company-lsp)
-(use-package helm-lsp :commands helm-lsp-workspace-symbol)
-(use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-;; optionally if you want to use debugger
-(use-package dap-mode)
-
-(use-package magit
-  :bind (("\C-c g g" . magit-status)
-         ("<f12>" . magit-status))
-  :config (setq magit-last-seen-setup-instructions "1.4.0"))
-
+;; Markdown
 (use-package markdown-mode
   :mode (("\\.text" . markdown-mode)
          ("\\.md" . markdown-mode)
          ("\\.markdown" . markdown-mode))
   :config
+  ;; TODO: Use custom-face for this
   ;; Make markdown look prettier
   (set-face-attribute 'markdown-header-face-1 nil :inherit 'markdown-header-face :height 2.0)
   (set-face-attribute 'markdown-header-face-2 nil :inherit 'markdown-header-face :height 1.5)
@@ -83,39 +146,7 @@
             (set-face-attribute 'org-level-2 nil :inherit 'outline-2 :foreground "#ffffff")
             (set-face-attribute 'org-level-3 nil :inherit 'outline-3 :foreground "#ffffff")))
 
-(use-package yasnippet
-  :ensure t
-  :config (yas-global-mode))
-
-(use-package yasnippet-snippets
-  :ensure t)
-
-(use-package diff-hl
-  :config (global-diff-hl-mode))
-
-(use-package popwin
-  :config (progn
-            (popwin-mode 1)
-            (global-set-key (kbd "C-z") popwin:keymap)))
-
-;; TODO: Express that this depends on helm-projectile?
-(use-package projectile
-  :ensure t
-  :bind ("C-c h" . helm-projectile)
-  :bind-keymap ("C-c p" . projectile-command-map)
-  :config
-  (projectile-global-mode)
-  (helm-projectile-on)
-  (setq projectile-use-git-grep 1))
-
-(use-package helm-ag
-  :ensure t)
-
-(use-package multiple-cursors
-  :bind (("C->" . mc/mark-next-like-this)
-         ("C-<" . mc/mark-previous-like-this)
-         ("C-c C-<" . mc/mark-all-like-this)
-         ("C-M-c" . mc/edit-lines)))
+;; Python
 
 (use-package python
   :ensure t)
@@ -127,6 +158,8 @@
 (use-package isortify
   :after python
   :hook (python-mode isortify-mode))
+
+;; Terraform
 
 (use-package terraform-mode
   :hook (terraform-mode . terraform-format-on-save-mode))
