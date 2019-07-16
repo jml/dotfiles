6408e4d9ce0bcabbcdcccd0e0279291ef7f75dc0
Jonathan Lange <jml@mumak.net>
2019-07-16 21:02:10 +0100

    Move environment configuration to packages

--- emacs/config.d/00-environment.el
+++
@@ -1,6 +0,0 @@
-;; TODO: Move this to packages after verifying that environment variables
-;; aren't needed earlier.
-(use-package exec-path-from-shell
-  :if (memq window-system '(mac ns))
-  :ensure t
-  :config (exec-path-from-shell-initialize))
--- emacs/config.d/20-packages.el
+++ emacs/config.d/20-packages.el
@@ -1,3 +1,7 @@
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :ensure t
+  :config (exec-path-from-shell-initialize))
 
 (use-package flycheck
   :config
