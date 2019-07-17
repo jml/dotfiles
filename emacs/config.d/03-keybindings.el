ea60e705e1311eb593491e72376538760009c498
Jonathan Lange <jml@mumak.net>
2019-07-17 17:08:36 +0100

    Remove unused keybindings
    We now use these for flymake instead.

--- emacs/config.d/03-keybindings.el
+++ emacs/config.d/03-keybindings.el
@@ -2,5 +2,3 @@
 (global-set-key "\M-g" 'goto-line)
 (global-set-key "\C-m" 'newline-and-indent) ;; enter key indents
 (global-set-key "\M-j" (lambda () (interactive) (delete-indentation 1)))
-(global-set-key "\M-n" 'scroll-up-line)
-(global-set-key "\M-p" 'scroll-down-line)
