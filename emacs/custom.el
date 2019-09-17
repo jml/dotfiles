8a816974f7032379f43c23498b9eb6032013ac61
Jonathan Lange <jml@mumak.net>
2019-09-17 15:02:07 +0100

    Give up on markdown faces

--- emacs/custom.el
+++ emacs/custom.el
@@ -127,4 +127,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(markdown-header-face-1 ((t (:inherit (quote markdown-header-face) :height 2.0))))
+ '(markdown-header-face-2 ((t (:inherit (quote markdown-header-face) :height 1.5))))
+ '(markdown-header-face-3 ((t (:inherit (quote markdown-header-face) :height 1.3))))
+ '(markdown-header-face-4 ((t (:inherit (quote markdown-header-face) :height 1.2))))
+ '(markdown-header-face-5 ((t (:inherit (quote markdown-header-face) :height 1.1)))))
