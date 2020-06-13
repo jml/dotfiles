ae658a35df6c21b8106fa7e1caf7a64c286dfecf
Jonathan Lange <jml@mumak.net>
2020-06-13 15:07:49 +0100

    Add solaire-mode

--- emacs/config.d/20-packages.el
+++ emacs/config.d/20-packages.el
@@ -11,6 +11,14 @@
 
 ;; Theme
 
+(use-package solaire-mode
+  :hook
+  ((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
+  (minibuffer-setup . solaire-mode-in-minibuffer)
+  :config
+  (solaire-global-mode +1)
+  (solaire-mode-swap-bg))
+
 ;; Other themes I have loved:
 ;; - monokai
 ;; - noctilux
