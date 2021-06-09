ea840eda2803bcfe5d3673a8fa521b223399bce0
Jonathan Lange <jml@mumak.net>
2021-06-09 12:47:57 +0100

    Update for solaire 2.0

--- emacs/config.d/20-packages.el
+++ emacs/config.d/20-packages.el
@@ -12,12 +12,10 @@
 ;; Theme
 
 (use-package solaire-mode
-  :hook
-  ((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
-  (minibuffer-setup . solaire-mode-in-minibuffer)
-  :config
-  (solaire-global-mode +1)
-  (solaire-mode-swap-bg))
+ :hook
+ ((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
+ :config
+ (solaire-global-mode +1))
 
 (use-package all-the-icons)
 
