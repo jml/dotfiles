163689dae01afb18392d8504cc075124650cf30a
Jonathan Lange <jml@mumak.net>
2021-06-28 16:02:54 +0100

    Configure Option as Option
    So I get £££!
    
    H/T @glyph

--- emacs/config.d/06-macos.el
+++ emacs/config.d/06-macos.el
@@ -1,4 +1,5 @@
 ;; Swap Command and Option when running in graphical mode on macOS
 ;; https://github.com/d12frosted/homebrew-emacs-plus/issues/178
 (when (and (eq system-type 'darwin) (display-graphic-p))
-  (cl-rotatef mac-command-modifier mac-option-modifier))
+  (cl-rotatef mac-command-modifier mac-option-modifier)
+  (setq mac-option-modifier 'option))
