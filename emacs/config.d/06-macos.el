e138c95fa9427e728d5f083b5cf178953f51b50e
Jonathan Lange <jml@mumak.net>
2022-01-24 11:51:28 +0000

    Give up on display-graphic-p

--- emacs/config.d/06-macos.el
+++ emacs/config.d/06-macos.el
@@ -1,5 +1,13 @@
 ;; Swap Command and Option when running in graphical mode on macOS
 ;; https://github.com/d12frosted/homebrew-emacs-plus/issues/178
-(when (and (eq system-type 'darwin) (display-graphic-p))
-  (cl-rotatef mac-command-modifier mac-option-modifier)
+
+;; This used to also check for (display-graphic-p), but because I always
+;; launch Emacs as a daemon, this was never true, and so I ended up with
+;; graphical frames with Command as <super> and Option as <meta>.
+;;
+;; What I *want* is for TTY frames to have Option as <meta> and graphical
+;; frames to have Command as <meta>, but that's out of my grasp right now.
+
+(when (eq system-type 'darwin)
+  (setq mac-command-modifier 'meta)
   (setq mac-option-modifier 'option))
