fa118b827c2bd402f5327d473f0a838663820618
Jonathan Lange <jml@mumak.net>
2022-06-02 14:08:26 +0100

    Add org-roam-ui

--- emacs/config.d/20-packages.el
+++ emacs/config.d/20-packages.el
@@ -414,6 +414,15 @@
   (org-roam-db-autosync-mode))
 
 
+(use-package org-roam-ui
+  :after org-roam
+  :hook (after-init . org-roam-ui-mode)
+  :config
+  (setq org-roam-ui-sync-theme t
+        org-roam-ui-follow t
+        org-roam-ui-update-on-save t
+        org-roam-ui-open-on-start t))
+
 ;; Python
 
 ;; Rust
