4c97bf349dc1a6b07ce9d443f6d9eaa9139e67c8
Jonathan Lange <jml@mumak.net>
2022-07-30 07:28:34 +0100

    Inherit fixed code size from surrounding face

--- emacs/config.d/01-appearance.el
+++ emacs/config.d/01-appearance.el
@@ -1,6 +1,6 @@
 ;; Use Adobe's Source Code Pro.
 (set-face-attribute 'default nil :height 110 :foundry "adobe" :family "Source Code Pro")
-(set-face-attribute 'fixed-pitch nil :height 110 :foundry "adobe" :family "Source Code Pro")
+(set-face-attribute 'fixed-pitch nil :foundry "adobe" :family "Source Code Pro")
 (set-face-attribute 'variable-pitch nil :height 140 :family "Source Sans Pro")
 
 ;; Don't show the startup screen
