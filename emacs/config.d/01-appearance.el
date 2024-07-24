dd38becdae72c1ce1535a5ad845159bdfe3a94d0
Jonathan Lange <jml@mumak.net>
2024-07-24 17:28:45 +0100

    Typographic changes
    - Font size of 12
    - Spacing between lines
    
    This brings the look more in line with VS Code,
    in particular, removing the cramped feeling I get when I switch to Emacs

--- emacs/config.d/01-appearance.el
+++ emacs/config.d/01-appearance.el
@@ -1,7 +1,11 @@
 ;; Use Adobe's Source Code Pro.
-(set-face-attribute 'default nil :height 110 :foundry "adobe" :family "Source Code Pro")
+(set-face-attribute 'default nil :height 120 :foundry "adobe" :family "Source Code Pro")
 (set-face-attribute 'fixed-pitch nil :foundry "adobe" :family "Source Code Pro")
 (set-face-attribute 'variable-pitch nil :height 140 :family "Source Sans Pro")
 
 ;; Don't show the startup screen
 (setq inhibit-startup-message t)
+
+;; Give us a bit of breathing room between lines.
+;; Experimenting shows that VS Code uses line spacing of 3.
+(setq-default line-spacing 2)
