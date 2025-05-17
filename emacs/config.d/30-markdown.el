e6070a5eb9aec24cec883400dbddd6c71ce0e17c
Jonathan Lange <jml@mumak.net>
2025-05-17 18:13:31 +0100

    Split Obsidian into a separate file

--- emacs/config.d/30-markdown.el
+++ emacs/config.d/30-markdown.el
@@ -14,20 +14,3 @@
   (markdown-table-face ((t (:inherit (shadow fixed-pitch)))))
   (markdown-code-face ((t (:inherit (shadow fixed-pitch)))))
   (markdown-pre-face ((t (:inherit (shadow fixed-pitch))))))
-
-
-(use-package obsidian
-  :custom
-  ;; Location of obsidian vault
-  (obsidian-directory "/Users/jml/Documents/Chainbrain")
-  ;; Default location for new notes from `obsidian-capture'
-  (obsidian-inbox-directory "Inbox")
-  ;; Useful if you're going to be using wiki links
-  (markdown-enable-wiki-links t))
-
-;; Obsidian TODO
-;; - move to new .el file
-;; - understand global-obsidian-mode and obsidian-backlinks-mode configuration
-;; - move obsidian-directory configuration to local.d, as it varies across laptops
-;; - set up Hydra
-;;   (define-key obsidian-mode-map (kbd "C-c M-o") 'obsidian-hydra/body)
---
+++ emacs/config.d/35-obsidian.el
@@ -0,0 +1,16 @@
+;; Obsidian
+(use-package obsidian
+  :after markdown
+  :custom
+  ;; Location of obsidian vault
+  (obsidian-directory "/Users/jml/Documents/Chainbrain")
+  ;; Default location for new notes from `obsidian-capture'
+  (obsidian-inbox-directory "Inbox")
+  ;; Useful if you're going to be using wiki links
+  (markdown-enable-wiki-links t))
+
+;; Obsidian TODO
+;; - understand global-obsidian-mode and obsidian-backlinks-mode configuration
+;; - move obsidian-directory configuration to local.d, as it varies across laptops
+;; - set up Hydra
+;;   (define-key obsidian-mode-map (kbd "C-c M-o") 'obsidian-hydra/body)
