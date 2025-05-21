36a4d9cec81f8d4dc0f155a93d6bb66c7087a6c5
Jonathan Lange <jml@mumak.net>
2025-05-21 11:48:15 -0400

    Fix fonts in markdown mode

--- emacs/config.d/30-markdown.el
+++ emacs/config.d/30-markdown.el
@@ -5,12 +5,15 @@
 
   :hook (markdown-mode . variable-pitch-mode)
 
-  :custom-face
-  (markdown-header-face-1 ((t (:inherit 'markdown-header-face :height 2.0))))
-  (markdown-header-face-2 ((t (:inherit 'markdown-header-face :height 1.5))))
-  (markdown-header-face-3 ((t (:inherit 'markdown-header-face :height 1.3))))
-  (markdown-header-face-4 ((t (:inherit 'markdown-header-face :height 1.2))))
-  (markdown-header-face-5 ((t (:inherit 'markdown-header-face :height 1.1))))
-  (markdown-table-face ((t (:inherit (shadow fixed-pitch)))))
-  (markdown-code-face ((t (:inherit (shadow fixed-pitch)))))
-  (markdown-pre-face ((t (:inherit (shadow fixed-pitch))))))
+  :config
+  ;; We have to do this in :config rather than :customize-face,
+  ;; otherwise it doesn't take effect. I don't understand the mechanism.
+  (custom-set-faces
+   '(markdown-header-face-1 ((t (:inherit 'markdown-header-face :height 2.0))))
+   '(markdown-header-face-2 ((t (:inherit 'markdown-header-face :height 1.5))))
+   '(markdown-header-face-3 ((t (:inherit 'markdown-header-face :height 1.3))))
+   '(markdown-header-face-4 ((t (:inherit 'markdown-header-face :height 1.2))))
+   '(markdown-header-face-5 ((t (:inherit 'markdown-header-face :height 1.1))))
+   '(markdown-table-face ((t (:inherit (shadow fixed-pitch)))))
+   '(markdown-code-face ((t (:inherit (shadow fixed-pitch)))))
+   '(markdown-pre-face ((t (:inherit (shadow fixed-pitch)))))))
--- emacs/custom.el
+++ emacs/custom.el
@@ -130,11 +130,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(markdown-header-face-1 ((t (:inherit 'markdown-header-face :height 2.0))))
- '(markdown-header-face-2 ((t (:inherit 'markdown-header-face :height 1.5))))
- '(markdown-header-face-3 ((t (:inherit 'markdown-header-face :height 1.3))))
- '(markdown-header-face-4 ((t (:inherit 'markdown-header-face :height 1.2))))
- '(markdown-header-face-5 ((t (:inherit 'markdown-header-face :height 1.1))))
  '(org-block ((t (:inherit fixed-pitch))))
  '(org-code ((t (:inherit (shadow fixed-pitch)))))
  '(org-document-info-keyword ((t (:inherit (shadow fixed-pitch)))))
