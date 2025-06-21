f7309228fa198f354765ad1fbef6df90afa21990
Jonathan Lange <jml@mumak.net>
2025-06-21 15:27:13 +0100

    Better relative paths in emacs init

--- configs/emacs/.emacs.d/init.el
+++ configs/emacs/.emacs.d/init.el
@@ -1,13 +1,20 @@
 ;; Customize
-(setq custom-file "~/.emacs.d/custom.el")
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 (load custom-file)
 
 ;; Load manually-managed third-party plugins.
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/plugins"))
+(add-to-list 'load-path (expand-file-name "plugins" user-emacs-directory))
 (require 'load-directory)
 
+;; Emacs seems to want a temp dir
+(let ((temp-dir (expand-file-name "temp" user-emacs-directory)))
+  (unless (file-directory-p temp-dir)
+    (make-directory temp-dir)))
+
 ;; Configuration that's only appropriate in this installation.
-(load-directory (expand-file-name "~/.emacs.d/local.d"))
+(let ((local-dir (expand-file-name "local.d" user-emacs-directory)))
+  (when (file-directory-p local-dir)
+    (load-directory local-dir)))
 
 ;; Configuration that's appropriate in all the places I use Emacs.
-(load-directory (expand-file-name "~/.emacs.d/config.d"))
+(load-directory (expand-file-name "config.d" user-emacs-directory))
