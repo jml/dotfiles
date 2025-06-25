1586fa6076c31e5455f6ab68c131f2828ff57765
Jonathan Lange <jml@mumak.net>
2025-06-25 14:22:34 +0100

    temp was a lie all along

--- configs/emacs/.emacs.d/config.d/04-backups.el
+++ configs/emacs/.emacs.d/config.d/04-backups.el
@@ -1,6 +1,5 @@
 
 ;; Make sure we're not storing files all over the place.
-(setq temporary-file-directory "~/.emacs.d/temp")
 (setq backup-directory-alist '((".*" . "~/.emacs.d/backups/")))
 (setq auto-save-list-file-prefix "~/.emacs.d/auto-save-list/.saves-")
 (setq auto-save-file-name-transforms
--- configs/emacs/.emacs.d/init.el
+++ configs/emacs/.emacs.d/init.el
@@ -3,11 +3,6 @@
 (add-to-list 'load-path (expand-file-name "plugins" user-emacs-directory))
 (require 'load-directory)
 
-;; Emacs seems to want a temp dir
-(let ((temp-dir (expand-file-name "temp" user-emacs-directory)))
-  (unless (file-directory-p temp-dir)
-    (make-directory temp-dir)))
-
 ;; Configuration that's only appropriate in this installation.
 (let ((local-dir (expand-file-name "local.d" user-emacs-directory)))
   (when (file-directory-p local-dir)
