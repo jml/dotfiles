18ae998165af4dbb218b8e1ab678198b0bcbf399
Jonathan Lange <jml@mumak.net>
2025-07-04 15:45:24 +0100

    Remove the title bar

---
+++ configs/emacs/.emacs.d/early-init.el
@@ -0,0 +1,4 @@
+;; Remove the title bar and borders
+;; Much more useful with tiling window managers
+;; Drop `-round` for straight edges.
+(add-to-list 'default-frame-alist '(undecorated-round . t))
