db745168d0d784361d4530eb1ee04f6c379dcc92
Jonathan Lange <jml@mumak.net>
2025-07-05 10:39:57 +0100

    Move obsidian config to local

--- configs/emacs/.emacs.d/config.d/35-obsidian.el
+++ configs/emacs/.emacs.d/config.d/35-obsidian.el
@@ -2,8 +2,6 @@
 (use-package obsidian
   :after (markdown elgrep)
   :custom
-  ;; Location of obsidian vault
-  (obsidian-directory "/Users/jml/Documents/Chainbrain")
   ;; Default location for new notes from `obsidian-capture'
   (obsidian-inbox-directory "Inbox")
   ;; Useful if you're going to be using wiki links
@@ -13,6 +11,5 @@
 
 ;; Obsidian TODO
 ;; - understand global-obsidian-mode and obsidian-backlinks-mode configuration
-;; - move obsidian-directory configuration to local.d, as it varies across laptops
 ;; - set up Hydra
 ;;   (define-key obsidian-mode-map (kbd "C-c M-o") 'obsidian-hydra/body)
