76d292db1290e9976c7dcdf7d9be4d9a6b2c5a39
Jonathan Lange <jml@mumak.net>
2025-10-04 19:16:22 +0100

    emacs: switch to vertico ecosystem

--- configs/emacs/.emacs.d/config.d/03-keybindings.el
+++ configs/emacs/.emacs.d/config.d/03-keybindings.el
@@ -1,4 +1,3 @@
 ;; Keybindings
-(global-set-key "\M-g" 'goto-line)
 (global-set-key "\C-m" 'newline-and-indent) ;; enter key indents
 (global-set-key "\M-j" (lambda () (interactive) (delete-indentation 1)))
--- configs/emacs/.emacs.d/config.d/20-navigation.el
+++ configs/emacs/.emacs.d/config.d/20-navigation.el
@@ -6,32 +6,71 @@
 
 ;;; Code:
 
-;; Helm - incremental completion and selection framework
-(use-package helm
-  :after diminish
-  :diminish helm-mode
-  :bind (("M-x" . helm-M-x)
-         ("C-x C-f" . helm-find-files)
-         ("C-x C-b" . helm-buffers-list))
-  :config (helm-mode 1))
+;; Vertico - vertical completion UI
+(use-package vertico
+  :init
+  (vertico-mode)
+  :config
+  ;; Cycle from bottom to top
+  (setq vertico-cycle t))
 
-;; Ripgrep integration for Helm
-(use-package helm-rg)
+;; Orderless - flexible completion matching
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
 
-;; Helm integration for Projectile
-(use-package helm-projectile)
+;; Consult - enhanced commands with completion
+(use-package consult
+  :bind (("C-x b" . consult-buffer)
+         ("C-x C-b" . consult-buffer)
+         ("C-x p b" . consult-project-buffer)
+         ("M-y" . consult-yank-pop)
+         ("M-g g" . consult-goto-line)
+         ("M-g M-g" . consult-goto-line)
+         ("M-g i" . consult-imenu)
+         ("M-s l" . consult-line)
+         ("M-s r" . consult-ripgrep)
+         ("M-s g" . consult-git-grep)
+         ("M-s f" . consult-find)
+         ("C-x C-f" . find-file))
+  :config
+  ;; Use project.el for Consult commands
+  (setq consult-project-function (lambda (_) (projectile-project-root)))
+  ;; Include hidden files in ripgrep searches, but exclude .git
+  (setq consult-ripgrep-args
+        "rg --null --line-buffered --color=never --max-columns=1000 --path-separator / --smart-case --no-heading --with-filename --line-number --search-zip --hidden --glob !.git"))
+
+;; Embark - context actions on completion candidates
+(use-package embark
+  :bind (("C-." . embark-act)
+         ("C-;" . embark-dwim)
+         ("C-h B" . embark-bindings))
+  :config
+  ;; Hide modeline when embark is active
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Embark integration with Consult
+(use-package embark-consult
+  :after (embark consult)
+  :hook (embark-collect-mode . consult-preview-at-point-mode))
 
 ;; Project management
 (use-package projectile
-  :after helm-projectile
-  :bind ("C-c h" . helm-projectile)
+  :bind (("C-c h" . projectile-find-file)
+         ("C-c p p" . projectile-switch-project))
   :bind-keymap ("C-c p" . projectile-command-map)
   :config
-  ;; If we don't have this, we get tonnes of crazy arguments on the command-line.
-  (setq helm-projectile-ignore-strategy 'search-tool)
-  (projectile-global-mode)
-  (helm-projectile-on)
+  (projectile-mode +1)
+  (setq projectile-completion-system 'default)
   (setq projectile-use-git-grep 1))
+
+;; M-x now uses Vertico with native execute-extended-command
+(global-set-key (kbd "M-x") 'execute-extended-command)
 
 ;; Rich annotations for completion
 (use-package marginalia
