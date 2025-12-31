27bae59164314144772b671c8528a75babde7cc1
Jonathan Lange <jml@mumak.net>
2025-12-31 10:24:43 +0000

    emacs: Writing package - olivetti

---
+++ configs/emacs/.emacs.d/config.d/30-writing.el
@@ -0,0 +1,12 @@
+;;; package --- Writing mode configuration
+;;; Commentary:
+;; Configuration for prose writing (novels, articles, etc.)
+
+;;; Code:
+
+;; Olivetti - Distraction-free writing with centered text
+(use-package olivetti
+  :config
+  (setq olivetti-body-width 80))
+
+;;; 30-writing.el ends here
